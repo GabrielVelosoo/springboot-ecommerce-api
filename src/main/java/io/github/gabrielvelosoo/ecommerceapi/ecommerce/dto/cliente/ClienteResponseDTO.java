572483bdb56a5c92fca8fb57682c498422252dac
Gabriel Veloso Pinheiro <tr26660es@trf1.gov.br>
2025-07-04 14:18:10 -0300

    feat(cliente): alterando retorno de dados do cliente dto

--- src/main/java/io/github/gabrielvelosoo/ecommerceapi/ecommerce/dto/cliente/ClienteResponseDTO.java
+++ src/main/java/io/github/gabrielvelosoo/ecommerceapi/ecommerce/dto/cliente/ClienteResponseDTO.java
@@ -1,6 +1,10 @@
 package io.github.gabrielvelosoo.ecommerceapi.ecommerce.dto.cliente;
 
-import java.time.LocalDateTime;
+import com.fasterxml.jackson.annotation.JsonFormat;
+import com.fasterxml.jackson.annotation.JsonGetter;
+import com.fasterxml.jackson.annotation.JsonIgnore;
+
+import java.time.LocalDate;
 
 public record ClienteResponseDTO(
         Long id,
@@ -8,8 +12,26 @@ public record ClienteResponseDTO(
         String senha,
         String nome,
         String sobrenome,
+
+        @JsonIgnore
         String cpf,
+
+        @JsonIgnore
         String cep,
-        LocalDateTime dataNascimento
+
+        @JsonFormat(pattern = "yyyy-MM-dd")
+        LocalDate dataNascimento
     ) {
+
+    @JsonGetter("cpf")
+    public String getCpfFormatado() {
+        if (cpf == null || cpf.length() != 11) return cpf;
+        return cpf.replaceAll("(\\d{3})(\\d{3})(\\d{3})(\\d{2})", "$1.$2.$3-$4");
+    }
+
+    @JsonGetter("cep")
+    public String getCepFormatado() {
+        if (cep == null || cep.length() != 8) return cep;
+        return cep.replaceAll("(\\d{5})(\\d{3})", "$1-$2");
+    }
 }
