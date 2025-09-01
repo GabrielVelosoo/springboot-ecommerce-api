f15b5078b00d1067082cb34c4a319e7fdd8a13b1
Gabriel Veloso Pinheiro <tr26660es@trf1.gov.br>
2025-09-01 15:41:28 -0300

    feat(kafka): separando as configs de consumer e producer do kafka

--- src/main/java/io/github/gabrielvelosoo/ecommerceapi/infraestrutura/kafka/config/KafkaConsumerConfiguration.java
+++ src/main/java/io/github/gabrielvelosoo/ecommerceapi/infraestrutura/kafka/config/KafkaConsumerConfiguration.java
@@ -2,36 +2,19 @@ package io.github.gabrielvelosoo.ecommerceapi.infraestrutura.kafka.config;
 
 import io.github.gabrielvelosoo.ecommerceapi.ecommerce.dto.event.PedidoCriadoEvent;
 import org.apache.kafka.clients.consumer.ConsumerConfig;
-import org.apache.kafka.clients.producer.ProducerConfig;
 import org.apache.kafka.common.serialization.StringDeserializer;
-import org.apache.kafka.common.serialization.StringSerializer;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.kafka.config.ConcurrentKafkaListenerContainerFactory;
-import org.springframework.kafka.core.*;
+import org.springframework.kafka.core.ConsumerFactory;
+import org.springframework.kafka.core.DefaultKafkaConsumerFactory;
 import org.springframework.kafka.support.serializer.JsonDeserializer;
-import org.springframework.kafka.support.serializer.JsonSerializer;
 
 import java.util.HashMap;
 import java.util.Map;
 
 @Configuration
-public class KafkaConfiguration {
-
-    @Bean
-    public ProducerFactory<String, PedidoCriadoEvent> producerFactory() {
-        Map<String, Object> configProps = new HashMap<>();
-        configProps.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, "localhost:9092");
-        configProps.put(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, StringSerializer.class);
-        configProps.put(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, JsonSerializer.class);
-        configProps.put(JsonSerializer.ADD_TYPE_INFO_HEADERS, false);
-        return new DefaultKafkaProducerFactory<>(configProps);
-    }
-
-    @Bean
-    public KafkaTemplate<String, PedidoCriadoEvent> kafkaTemplate() {
-        return new KafkaTemplate<>(producerFactory());
-    }
+public class KafkaConsumerConfiguration {
 
     @Bean
     public ConsumerFactory<String, PedidoCriadoEvent> consumerFactory() {
