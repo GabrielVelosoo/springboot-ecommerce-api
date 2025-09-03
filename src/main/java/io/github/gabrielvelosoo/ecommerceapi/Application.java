f2b0e771cb1da9cbaa5dd8800a93c68240682141
Gabriel Veloso <gaabrielvelooso@gmail.com>
2025-09-03 20:26:24 -0300

    feat(server): adicionando WildFly na aplicação

--- src/main/java/io/github/gabrielvelosoo/ecommerceapi/Application.java
+++ src/main/java/io/github/gabrielvelosoo/ecommerceapi/Application.java
@@ -3,13 +3,19 @@ package io.github.gabrielvelosoo.ecommerceapi;
 import org.springframework.boot.Banner;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import org.springframework.context.ConfigurableApplicationContext;
 import org.springframework.core.env.ConfigurableEnvironment;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
 @EnableJpaAuditing
-public class Application {
+public class Application extends SpringBootServletInitializer {
+
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+        return application.sources(Application.class);
+    }
 
     public static void main(String[] args) {
         SpringApplicationBuilder builder = new SpringApplicationBuilder(Application.class);
@@ -20,5 +26,4 @@ public class Application {
         String applicationName = environment.getProperty("spring.application.name");
         System.out.println("Nome da aplicação: " + applicationName);
     }
-
 }
